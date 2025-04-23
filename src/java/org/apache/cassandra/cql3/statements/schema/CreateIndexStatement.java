6fc81529fac7e23fe81ae20b6020c178b4dd65cf
Ruslan Fomkin <ruslan.fomkin@gmail.com>
2025-04-23 10:37:21 +0200

    Comment with future fix CASSANDRA-20235

--- src/java/org/apache/cassandra/cql3/statements/schema/CreateIndexStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/schema/CreateIndexStatement.java
@@ -237,6 +237,7 @@ public final class CreateIndexStatement extends AlterSchemaStatement
 
         AbstractType<?> baseType = column.type.unwrap();
 
+        // This check will be removed with CASSANDRA-20235
         if ((kind == IndexMetadata.Kind.CUSTOM))
             validateCustomIndexColumnName(target.column.toString());
 
