116104d4b17a20fe09ce6318e6d0e5d1d6836c15
Stefan Miklosovic <smiklosovic@apache.org>
2025-10-30 16:04:16 +0100

    do assertion instead of "if"

--- src/java/org/apache/cassandra/net/OutboundConnection.java
+++ src/java/org/apache/cassandra/net/OutboundConnection.java
@@ -1127,39 +1127,45 @@ public class OutboundConnection
                         // it is expected that close, if successful, has already cancelled us; so we do not need to worry about leaking connections
                         assert !state.isClosed();
 
-                        if (result.success() instanceof MessagingSuccess) {
-                            MessagingSuccess success = (MessagingSuccess) result.success();
-                            debug.onConnect(success.messagingVersion, settings);
-                            state.disconnected().maintenance.cancel(false);
+                        Result.Success resultSuccess = result.success();
+                        assert resultSuccess instanceof MessagingSuccess;
 
-                            FrameEncoder.PayloadAllocator payloadAllocator = success.allocator;
-                            Channel channel = success.channel;
-                            Established established = new Established(success.messagingVersion, channel, payloadAllocator, settings);
-                            state = established;
-                            channel.pipeline().addLast("handleExceptionalStates", new ChannelInboundHandlerAdapter() {
-                                @Override
-                                public void channelInactive(ChannelHandlerContext ctx) {
-                                    disconnectNow(established);
-                                    ctx.fireChannelInactive();
+                        MessagingSuccess success = (MessagingSuccess) resultSuccess;
+                        debug.onConnect(success.messagingVersion, settings);
+                        state.disconnected().maintenance.cancel(false);
+
+                        FrameEncoder.PayloadAllocator payloadAllocator = success.allocator;
+                        Channel channel = success.channel;
+                        Established established = new Established(success.messagingVersion, channel, payloadAllocator, settings);
+                        state = established;
+                        channel.pipeline().addLast("handleExceptionalStates", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelInactive(ChannelHandlerContext ctx)
+                            {
+                                disconnectNow(established);
+                                ctx.fireChannelInactive();
+                            }
+
+                            @Override
+                            public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+                            {
+                                try
+                                {
+                                    invalidateChannel(established, cause);
                                 }
-
-                                @Override
-                                public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-                                    try {
-                                        invalidateChannel(established, cause);
-                                    } catch (Throwable t) {
-                                        logger.error("Unexpected exception in {}.exceptionCaught", this.getClass().getSimpleName(), t);
-                                    }
+                                catch (Throwable t)
+                                {
+                                    logger.error("Unexpected exception in {}.exceptionCaught", this.getClass().getSimpleName(), t);
                                 }
-                            });
-                            ++successfulConnections;
+                            }
+                        });
+                        ++successfulConnections;
 
-                            logger.info("{} successfully connected, version = {}, framing = {}, encryption = {}",
+                        logger.info("{} successfully connected, version = {}, framing = {}, encryption = {}",
                                     id(true),
                                     success.messagingVersion,
                                     settings.framing,
                                     encryptionConnectionSummary(channel));
-                        }
                         break;
 
                     case RETRY:
